c700f9e44fbe533f1a1cc1e558dbb108ad7081c4
Daniel McBrayer <dmcbrayer@gmail.com>
2014-07-23 23:05:06 -0400

    Fix a typo in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,12 +68,12 @@ Rails.application.configure do
   #mail delivery options
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address    => 'smtp.sendgrid.net',
-    :port       => '587'
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
     :authentication => :plain,
-    :user_name  => ENV['SENDGRID_USERNAME'],
-    :password   => ENV['SENDGRID_PASSWORD'],
-    :domain     => 'heroku.com',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
 
